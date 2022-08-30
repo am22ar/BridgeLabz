22690c5d751fe1303a28b247cbf928d4bd9048bd
am22ar <amarpatil12222@gmail.com>
2022-08-30 12:11:41 +0530

    [AMAR]-Program to Calculate Emp wage using for loop

---
+++ UC6EmpWageUsingWhileLoop.sh
@@ -0,0 +1,28 @@
+#!/bin/bash -x
+isPartTime=1
+isFullTime=2
+empRatePerHr=20
+numWorkingDays=20
+maxWorkingHrs=10
+
+totalEmpHrs=0
+totalWorkingDays=0
+
+while [[ $totalEmpHrs -lt $maxWorkingHrs && $totalWorkingDays -lt $numWorkingDays ]]
+do
+((totalWorkingDays++))
+empCheck=$((RANDOM%3))
+case $empCheck in
+$isFullTime)
+	empHrs=8
+	;;
+$isPartTime)
+	empHrs=4
+	;;
+	  *)
+	empHrs=0
+	;;
+esac
+totalEmpHrs=$(( $empHrs + $totalEmpHrs))
+done
+totalSalary=$(( $totalEmpHrs * $empRatePerHr ))
